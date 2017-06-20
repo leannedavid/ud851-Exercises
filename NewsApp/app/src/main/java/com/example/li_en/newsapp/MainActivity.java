b05731d53bf5c66a6c06d4d33b0e62cc72622ae5
leannedavid <leanne.skit@gmail.com>
2017-06-20 14:29:30 -0700

    finished newsapp

---
+++ NewsApp/app/src/main/java/com/example/li_en/newsapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.li_en.newsapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.li_en.newsapp.utilities.NetworkUtils;
+
+import org.w3c.dom.Text;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mNewsTextView;
+    private TextView mUrlDisplayTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNewsTextView = (TextView) findViewById(R.id.news_search_results);
+        //mUrlDisplayTextView = (TextView) findViewById(R.id.url_display);
+    }
+
+    private void loadNewsData(){
+        /* Was used to test if the URL was built correctly */
+        //URL testNews = NetworkUtils.buildUrl(NetworkUtils.get_API_Key());
+        //mUrlDisplayTextView.setText(testNews.toString());
+        new FetchNewsTask().execute(NetworkUtils.get_API_Key());
+    }
+
+
+
+    public class FetchNewsTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params){
+            if(params.length == 0){
+                return null;
+            }
+
+            String api = params[0];
+            URL newsRequestUrl = NetworkUtils.buildUrl(api);
+
+            String jsonNewsDataResponse = null;
+
+            try{
+                jsonNewsDataResponse = NetworkUtils.getResponseFromHttpUrl(newsRequestUrl);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return jsonNewsDataResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String newsData){
+            if(newsData != null && !newsData.equals("")) {
+                mNewsTextView.setText(newsData);
+//                for (String news : newsData){
+//                    mNewsTextView.append(news + "\n\n");
+//                }
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.news, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_search){
+            mNewsTextView.setText("");
+            loadNewsData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- ud851-Sunshine/S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ ud851-Sunshine/S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -101,28 +101,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     // COMPLETED (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+    // COMPLETED (6) Return true to display the menu
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
         MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
         inflater.inflate(R.menu.forecast, menu);
-
-        // COMPLETED (6) Return true to display the menu
+        /* Return true so that the menu is displayed in the Toolbar */
         return true;
     }
 
     // COMPLETED (7) Override onOptionsItemSelected to handle clicks on the refresh button
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_refresh){
+        if (id == R.id.action_refresh) {
             mWeatherTextView.setText("");
             loadWeatherData();
-
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
